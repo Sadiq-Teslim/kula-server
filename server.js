7177ec8bfaee9629aa3ecebc604a3b7155cfc885
Sadiq Teslim <sadiqadetola08@gmail.com>
2025-07-26 21:00:31 +0100

    render deployment preparation

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path'); // Added Path module
 // --- Initialize App ---
 const app = express();
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 // --- Middleware ---
